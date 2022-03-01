4a185855e61d6df91b65cef9793349de3086f46c
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-01 10:11:24 -0300

    acrescentando algumas funções

---
+++ aprendendo-R.R
@@ -0,0 +1,52 @@
+5+125
+seq(1,100,by=2)
+200-10
+200:5
+15*5
+5^2
+5%%2
+1:10
+
+1:100
+300:400
+seq(1,100,by =2)
+3+2*1-2*1
+2+8*5 -1
+4/2+3*5+4-2
+(4/2)+(3*5)+(4-2)
+
+a= 5
+a
+a <- 5
+a
+a= a^2
+a= "Joao"
+a
+a = TRUE
+a
+
+a= cars
+View(a)
+d= Titanic
+View(d)
+a= list("familia", 4,"Joao, Erica, Gigio e Cookie")
+a
+c= list(a,d)
+c
+str(c)
+
+a
+print(a)
+cat(a)
+(a)
+
+m = mean(1:2)
+
+notas= c(4,5,6,7)
+media.turma = mean(notas)
+media.turma1 = mean(notas)
+media.Turma = mean(notas)
+Media.Turma = mean(notas)
+m.t1 = mean(notas)
+
+
