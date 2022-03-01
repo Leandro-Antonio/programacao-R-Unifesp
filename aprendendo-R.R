1fae39041be2ba2b15f036b96ceb5a2e4e6580a2
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-01 10:15:19 -0300

    usando ajuda do R e trabalhando com tipos de dados

--- aprendendo-R.R
+++ aprendendo-R.R
@@ -49,4 +49,56 @@ media.Turma = mean(notas)
 Media.Turma = mean(notas)
 m.t1 = mean(notas)
 
+help("sum")
+?sum
+??sum
+
+vector1= 1:10
+vector1*5
+
+
+vector1 <- 1:10
+typeof(vector1)
+
+
+vector2= c(1,2,3,4,5,6,7,8,9,10)
+typeof(vector2)
+
+vector3= c(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L)
+typeof(vector3)
+ls()
+?ls
+
+
+vector4 = c("Joao", "Erica","Gigio","Cookie")
+typeof(vector4)
+
+vector5= c(1,2,"3")
+typeof(vector5)
+
+
+vector6= c(a, b,c,d)
+
+a = "Joao"
+b=  "Erica"
+c=  "Gigio"
+d= "Cookie"
+vector6= c(a, b,c,d)
+vector6
+
+vector7= c(T,F,T)
+typeof(vector7)
+vector8= c(TRUE,FALSE,TRUE)
+typeof(vector8)
+
+vector8= c(True,False,True)
+
+x= 3+4i
+typeof(x)
+
+var.a = intToBits(1)
+var.a
+var.b = intToBits(2)
+var.b
+typeof(var.a)
 
