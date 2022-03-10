7f9dd7ae1929b98c2b1c4105c05ed699eb5c45a9
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-10 13:46:55 -0300

    Usa função names no vetor notas, atribui NULL para anular função

---
+++ aula9_vetores_e_atributos.R
@@ -0,0 +1,11 @@
+#Aula 9 - Vetores e atributos (criando nomes de vetores)
+
+notas = c(5, 6, 7, 10)
+notas
+
+names(notas) = c("João", "Maria", "Jose", "Paulo")
+notas
+
+# NULL atribui um valor nulo na função names
+names(notas) = NULL
+notas
