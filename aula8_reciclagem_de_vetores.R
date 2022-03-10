8517e9feecb7afaec9e841fd68fbae5bc1385499
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-10 12:14:31 -0300

    Estudando reciclagem de vetores - operação com vetores de tamanhos diferentes

---
+++ aula8_reciclagem_de_vetores.R
@@ -0,0 +1,20 @@
+# Aula 8 - Reciclagem de vetores
+vetor_a = c(1, 2, 3, 4)
+vetor_b = c(1, 2, 3, 4, 5)
+vetor_c = vetor_a + vetor_b
+# vetor_c = c(2,4,6,8,6)
+# O R comecou a repetir os valores para poder somar!!!
+# vetor_a =c(1,2,3,4,1) ---repetiu o primeiro valor para completar
+# vetor_b =c(1,2,3,4,5)
+# Vetor_c = c(2,4,6,8,6)
+
+vetor_a = c(1,2,3,4)
+vetor_b = c(1,2,3,4,5,6,7,8)
+vetor_c = vetor_a + vetor_b
+vetor_c
+
+# vetro_c = c(2,4,6,8,6,10,12)
+# O R comecou a repetir os valores para poder somar!!!!
+# vetor_a = c(1,2,3,4,1,2,3,4)
+# vetor_b = c(1,2,3,4,5,6,7,8)
+# vetor_c = c(2,4,6,8,6,8,9,12)
