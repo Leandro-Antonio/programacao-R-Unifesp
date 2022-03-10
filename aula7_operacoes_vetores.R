fca584f596c2fac1f441827fcb82875306f5b0ee
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-10 11:34:44 -0300

    Adicionado funcões média, min, max

---
+++ aula7_operacoes_vetores.R
@@ -0,0 +1,17 @@
+#Vetores e pereções com vetores
+
+vetor_a = c(1, 2, 3, 4) # c fala-se combine. 
+vetor_b = c(2, 4, 6, 8)
+
+vetor_c = vetor_a + vetor_b
+vetor_d = vetor_b - vetor_a
+vetor_e = vetor_b * vetor_a
+vetor_f = vetor_b / vetor_a
+
+soma = sum(vetor_a)
+produto = prod(vetor_a)
+media = mean(vetor_a)
+min(vetor_a)
+max(vetor_a)
+
+# Card de referência https://cran.r-project.org/doc/contrib/Short-refcard.pdf
